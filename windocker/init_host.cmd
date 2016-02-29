896ff43c607f6627685f05c900c4215bf7f207bb
Ali Sadık Kumlalı <as_kumlali@yahoo.com>
2016-02-29 16:30:56 +0200

    Bug fixed.
    60 seconds wait period added before restarting VM so that changes made in VM persisted.

--- windocker/init_host.cmd
+++ windocker/init_host.cmd
@@ -9,9 +9,21 @@ rem is deleted and a new VM(default) is created, then the script has
 rem to be run again.
 rem --------------------------------------------------------------------
 call conf.cmd
-echo Configuration file(conf.cmd) has been loaded.
+echo [windocker] Configuration file(conf.cmd) has been loaded.
 
 set WINDOCKER_HOME=/c/Users/%USERNAME%/windocker
 
 docker-machine ssh default "export WINDOCKER_HOME=%WINDOCKER_HOME%;export HTTP_PROXY=%HTTP_PROXY%;export HTTPS_PROXY=%HTTPS_PROXY%;export NO_PROXY=%NO_PROXY%;%WINDOCKER_HOME%/init_host.sh"
+
+echo [windocker] Proxy configuration and certificates are persisted into Boot2Docker VM. 
+echo [windocker] VM will be rebooted in 60 seconds to let Boot2Docker to load certificates.
+
+rem This hack let us wait 60 seconds before restarting VM. 
+rem In some environments it is necessary. Otherwise changes made in
+rem /var/lib/boot2docker/profile are not persisted.
+ping 127.0.0.1 -n 61 > nul
+
 docker-machine restart default
+
+rem Was changes persisted, really?
+rem docker-machine ssh default "%WINDOCKER_HOME%/init_host.sh showWindockerChanges"
