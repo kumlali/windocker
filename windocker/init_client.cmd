6eee894a3eb3d33f10bd37062568282b7ca18d0b
Ali Sadık Kumlalı <as_kumlali@yahoo.com>
2016-02-29 16:27:42 +0200

    Output made more readable.

--- windocker/init_client.cmd
+++ windocker/init_client.cmd
@@ -11,20 +11,21 @@ rem Otherwise, Docker commands will not work correctly.
 rem --------------------------------------------------------------------
 
 call conf.cmd
-echo Configuration file(conf.cmd) has been loaded.
+echo [windocker] Configuration file(conf.cmd) has been loaded.
 
 rem Requests from Docker client in Windows to Docker daemon in B2D VM 
 rem should not pass through proxy. '--no-proxy' causes IP of B2D VM 
 rem to be added to the end of NO_PROXY variable.
 for /f "tokens=*" %%i in ('docker-machine env --no-proxy default') do %%i
 
-echo Environment variables have been set:
-echo HTTP_PROXY         : %HTTP_PROXY%
-echo HTTPS_PROXY        : %HTTPS_PROXY%
-echo NO_PROXY           : %NO_PROXY%
-echo DOCKER_HOST        : %DOCKER_HOST%
-echo DOCKER_MACHINE_NAME: %DOCKER_MACHINE_NAME%
-echo DOCKER_TLS_VERIFY  : %DOCKER_TLS_VERIFY%
-echo DOCKER_CERT_PATH   : %DOCKER_CERT_PATH%
+echo [windocker] Environment variables have been set:
+echo [windocker]   HTTP_PROXY         : %HTTP_PROXY%
+echo [windocker]   HTTPS_PROXY        : %HTTPS_PROXY%
+echo [windocker]   NO_PROXY           : %NO_PROXY%
+echo [windocker]   DOCKER_HOST        : %DOCKER_HOST%
+echo [windocker]   DOCKER_MACHINE_NAME: %DOCKER_MACHINE_NAME%
+echo [windocker]   DOCKER_TLS_VERIFY  : %DOCKER_TLS_VERIFY%
+echo [windocker]   DOCKER_CERT_PATH   : %DOCKER_CERT_PATH%
 echo.
-echo Command prompt has been initialized. Try 'docker run hello-world'.
+echo [windocker] Command prompt has been initialized.
+echo [windocker] Try 'docker run hello-world'.
